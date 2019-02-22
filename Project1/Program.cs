71b60d125031c35dbe3d63d2a00543b6bbd6ebd3
acdc@plaz.com.ua <acdc@plaz.com.ua>
2019-02-22 17:14:29 +0200

    Add Console.WriteLine("UPS")

---
+++ Project1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Namespace - объявления области действия, которая содержит набор связанных объектов.
+Пространство имен можно использовать для организации элементов кода и
+для создания глобально уникальных типов. */
+
+namespace Project1
+{
+    // Объявление класса Program
+    class Program
+    {
+        //Вызов метода Main
+        static void Main(string[] args)
+        {
+            //Метод Writeline выводит информацию на экран
+            Console.WriteLine("Первая программа на С#");
+
+            //Метод ReadLine читает все символы в строке из стандартного ввода
+            Console.ReadLine();
+
+        }
+    }
+}
+
