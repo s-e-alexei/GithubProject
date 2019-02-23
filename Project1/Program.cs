fdf6fe2aaa4ec40043b5c79a52a6f2d3476d1b77
acdc@plaz.com.ua <acdc@plaz.com.ua>
2019-02-23 17:10:08 +0200

    Доблен цикл, исправлены ошибки.

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -13,16 +13,51 @@ namespace Project1
     // Объявление класса Program
     class Program
     {
+        private static readonly object console;
+
         //Вызов метода Main
         static void Main(string[] args)
         {
             //Метод Writeline выводит информацию на экран
             Console.WriteLine("Первая программа на С#");
-
-            //Метод ReadLine читает все символы в строке из стандартного ввода
             Console.ReadLine();
 
-        }
-    }
-}
+            Console.WriteLine("UPS");
+            Console.ReadLine(); /*Метод ReadLine читает все символы
+                                    в строке из стандартного ввода*/
 
+
+            int a = 1;
+            {
+                Console.WriteLine("a: " + a);
+                Console.ReadLine();
+            }
+
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Console.ReadKey();
+            }
+
+            Console.WriteLine();
+
+            string[] Arr = new string[] { "Одесский", "Национальный", "Политехнический", "Университет", };
+            int p = 0;
+            while (p < 4)
+            {
+                Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет           
+                Console.WriteLine(Arr[p]);
+                p++;
+                Console.ReadKey();
+                Console.ResetColor(); // сбрасываем в стандартный          
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red; // устанавливаем цвет 
+            Console.WriteLine("Конец");
+            Console.ReadKey();
+            Console.ResetColor(); // сбрасываем в стандартный
+            
+        }
+    }    
+}
